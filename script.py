19c8c0fb443c3956eff850d6fd69bb50505ecc61
Alan Vale <84345906+alaanvv@users.noreply.github.com>
2022-03-31 16:23:18 -0300

    Add files via upload
    Agora aceitando links de playlist

--- script.py
+++ script.py
@@ -1,11 +1,28 @@
-from pytube import YouTube
+from pytube import YouTube, Playlist
 
 link = input('Link:\n|> ')
-yt = YouTube(link)
 
 type_mode = input('\n\nType:\n[1] Video\n[2] Audio\n|> ')
 
 if type_mode == '1':
-    yt.streams.get_highest_resolution().download()
+    
+    if 'playlist' in link:
+        for yt in Playlist(link).videos:
+            try: print(yt.title) # os nomes dos vídeos podem ter caracteres desconhecidos
+            except: pass
+
+            yt.streams.get_highest_resolution().download()
+            
+    else:
+        yt = YouTube(link)
+        yt.streams.get_highest_resolution().download()
 else:
-    yt.streams.get_audio_only().download()
+    if 'playlist' in link:
+        for yt in Playlist(link).videos:
+            try: print(yt.title)
+            except: pass
+            yt.streams.get_audio_only().download()
+            
+    else:
+        yt = YouTube(link)
+        yt.streams.get_audio_only().download()
