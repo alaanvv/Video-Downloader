fd106de9b72d1024bb5f98c8d74b20fe74e31764
Alan Vale <84345906+alaanvv@users.noreply.github.com>
2022-04-02 11:17:31 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,28 +1,111 @@
+''' Doc 
+    Nesse código eu usei um sistema que
+    eu considero organizado e me facilita
+    a ler e mudar coisas, não sei se é uma
+    maneira muito responsável, mas eu 
+    distribuo tudo dentro de funções.
+
+    Funções com o sufixo 'tree' servem pra 
+    direcionar o algoritmo e fazer pequenas
+    ações de acordo com as necessidades, se
+    dividindo como galhos de uma árvore.
+'''
+# ---
+#region Importando bibliotecas 
+
 from pytube import YouTube, Playlist
+from os import rename, mkdir
 
-link = input('Link:\n|> ')
+#endregion
+# ---
+#region Funções 
 
-type_mode = input('\n\nType:\n[1] Video\n[2] Audio\n|> ')
+def start():
+    '''
+    Preparando o terreno e iniciando.
+    '''
+    link = input('\nLink:\n|> ')
 
-if type_mode == '1':
+    mode = input('\nType:\n[1] Video mp4\n[2] Audio mp4\n[3] Audio mp3\n|> ')
+
+    try: mkdir('downloads') # Criando uma pasta pra guardar os arquivos gerados
+    except: pass
     
-    if 'playlist' in link:
-        for yt in Playlist(link).videos:
-            try: print(yt.title) # os nomes dos vídeos podem ter caracteres desconhecidos
+    mode_tree(link, mode)
+
+    if input('\nRestart: (y/n)\n|> ') == 'y': start() # Opção pra recomeçar sem ter que reabrir o exe
+
+def mode_tree(link, mode):
+    '''
+    Separa playlists de videos
+    '''
+
+    if 'playlist' in link: playlist_tree(link, mode)
+    else: video_tree(link, mode)
+
+def video_tree(link, mode):
+    '''
+    Envia o objeto YouTube para a função de download
+    '''
+
+    yt = YouTube(link)
+
+    if mode == '1':
+        download_video(yt)
+
+    elif mode == '2':        
+        download_audio(yt)
+
+    else:
+        file = download_audio(yt)
+        try: rename(file, file[:-3] + 'mp3')
+        except: pass
+
+def playlist_tree(link, mode):
+    '''
+    Itera sobre os vídeos da playlist os enviando para a função de download
+    '''
+
+    for yt in Playlist(link).videos:
+
+        if mode == '1': 
+            download_video(yt)
+
+        elif mode == '1': 
+            download_audio(yt)
+
+        else: 
+            file = download_audio(yt)
+            try: rename(file, file[:-3] + 'mp3')
             except: pass
 
-            yt.streams.get_highest_resolution().download()
-            
-    else:
-        yt = YouTube(link)
-        yt.streams.get_highest_resolution().download()
-else:
-    if 'playlist' in link:
-        for yt in Playlist(link).videos:
-            try: print(yt.title)
-            except: pass
-            yt.streams.get_audio_only().download()
-            
-    else:
-        yt = YouTube(link)
-        yt.streams.get_audio_only().download()
+def title(yt):
+    '''
+    Printa o título do vídeo
+    '''
+
+    try: print('\n', yt.title) # Nomes dos vídeos podem ter caracteres desconhecidos que gerariam erros
+    except: pass
+
+def download_video(yt):
+    '''
+    Baixa usando o método get_highest_resolution
+    e retorna o arquivo final
+    '''
+
+    title(yt)
+    return yt.streams.get_highest_resolution().download('downloads')
+    
+def download_audio(yt):
+    '''
+    Baixa usando o método get_audio_only
+    e retorna o arquivo final
+    '''
+
+    title(yt)
+    return yt.streams.get_audio_only().download('downloads')
+
+#endregion
+# ---
+
+start()
